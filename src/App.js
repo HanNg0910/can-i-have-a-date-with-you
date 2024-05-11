3cdb3151a3f4129cc459500ca4daa8601121a16c
Han Nguyen <hannguyen@Hans-MacBook-Pro.local>
2024-05-12 02:39:43 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<AskOut />} />
+          <Route path="/can-i-have-a-date-with-you" element={<AskOut />} />
           <Route path="/yes" element={<Yes/>} />
       </Routes>
   </Router>
